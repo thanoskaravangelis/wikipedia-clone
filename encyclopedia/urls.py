e7bc3592edb90df11ae50ec94aae87d790ff7f3e
thanoskaravangelis <thanosblv@gmail.com>
2021-05-01 15:50:46 +0300

    Add view that renders search results based on substrings

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name="encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("/results", views.searching, name="searching")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -18,3 +18,35 @@ def entry(request,title):
             "title": title,
             "content": markdown2.markdown(util.get_entry(title))
         })
+
+
+def searching(request):
+    entries = util.list_entries()
+    entries_lower = [s.lower() for s in entries]
+    entries_dict = dict(zip(entries_lower,entries))
+    
+    if 'q' in request.GET:
+        search_str = request.GET['q']
+    else: 
+        return render(request, "encyclopedia/results_error.html",{
+            "term":""
+        })
+
+    if search_str in entries :
+        return entry(request,search_str)
+    elif search_str in entries_lower :
+        return entry(request, entries_dict[search_str])
+    else:
+        results = [s for s in entries_lower if search_str.lower() in s]
+        if results:
+            return render(request, "encyclopedia/results.html", {
+                "results":[entries_dict[s] for s in results],
+                "term":search_str
+            })
+        else:
+            return render(request, "encyclopedia/results_error.html", {
+                "term":search_str
+            })
+
+
+        
