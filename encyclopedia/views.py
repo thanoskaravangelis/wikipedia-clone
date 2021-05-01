803ada3c89e1bbcf578d334ac2663b63357c37b6
thanoskaravangelis <thanosblv@gmail.com>
2021-05-01 14:05:25 +0300

    Add view that renders an entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import markdown2
 
 from . import util
 
@@ -8,3 +10,11 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request,title):
+    if title not in util.list_entries():
+        return HttpResponse("Error")
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": markdown2.markdown(util.get_entry(title))
+        })
